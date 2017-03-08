5634b1f3a31af151751c43a68c133d769f63255c
overkektus <egor.piskunou@gmail.com>
2017-03-08 17:17:45 +0300

    lesson 2
    Data type

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log('hello from main.js')
+/*
+var x;
+var strings = 'string';
+var another_strings = "another string";
+var booleanType = (x > 5);
+console.log(x);
+*/
+
+var owerFirstObject = {x: 5, y: 'string'}
+console.log(owerFirstObject.x);
+owerFirstObject.x = 'new string';
+console.log(owerFirstObject.x);
