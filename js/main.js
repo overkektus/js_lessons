e5f50a422b09869270c682f2f1f201c8ded4722d
overkektus <egor.piskunou@gmail.com>
2017-03-10 17:58:55 +0300

    lesson 9
    Arrays

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
-//Closures
-function counter() {
-   var a = 0;
-   return function add() {
-      a++;
-      console.log(a);
-   }
+//Arrays
+var fruits = ['apple', 'lemon', 'orange'];
+//var fruits2 = new Array('apple2', 'orange2');
+//console.log(fruits2.length);
+
+console.log(fruits.length);
+fruits[3] = 'pineapple';
+console.log(fruits.length);
+console.log(fruits[3]);
+
+fruits.push(4);
+console.log(fruits);
+
+var fruits = ['apple', 'lemon', 'orange'];
+for (var i = 0; i < fruits.length; i++) {
+   console.log(fruits[i]);
+   fruits[i] = '2';
 }
 
-var plus = counter();
-
-plus();
-plus();
-plus();
-plus();
-
-console.log(a);
+console.log(fruits.length);
+console.log(fruits);
+fruits[20] = 'another element';
+console.log(fruits);
+console.log(fruits.length);
