0a0ece5513fba1bd97d45630d5b63373c0511f1b
overkektus <egor.piskunou@gmail.com>
2017-03-18 17:01:03 +0300

    Host objects

--- js/main.js
+++ js/main.js
@@ -1,19 +1,8 @@
-// Date object
-console.log(new Date());
-var birthday = new Date("June 16 1993");
-console.log(birthday);
-var birthday2 = new Date(1993, 5, 16, 22);
-console.log(birthday2);
-
-// .now
-console.log(Date.now());
-
-// .parse
-console.log(Date.parse("Wed Jun 16 1968"));
-
-// .UTC
-var birthday3 = new Date(Date.UTC(1993, 5, 16, 22));
-console.log(birthday3);
-
-var dateAlert = "Today is " + birthday.getDay() + " " + birthday.getFullYear()+" and month is " + (birthday.getMonth() + 1);
-console.log(dateAlert);
+// window object - global
+var a = 'Vlad';
+window.console.log(a);
+console.log(window.navigator);
+console.log(window.screen);
+console.log(window.location);
+console.log(window.history);
+console.log(window.document);
