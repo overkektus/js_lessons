aaf024e7493560fe13649fc6cc7798e0165e19fe
overkektus <egor.piskunou@gmail.com>
2017-03-18 20:02:23 +0300

    Document

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// window object - global
-var a = 'Vlad';
-window.console.log(a);
-console.log(window.navigator);
-console.log(window.screen);
-console.log(window.location);
-console.log(window.history);
-console.log(window.document);
